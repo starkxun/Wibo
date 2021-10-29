b322ece06b316900697861a289169f89ac876695
starkxun <wx15056446255@gmail.com>
2021-10-29 23:38:16 +0800

    切割头部和尾部子视图

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
+| 为页面指定URL
 */
 
-Route::get('/',"StaticPagesController@home");
-Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController@about');
+Route::get('/',"StaticPagesController@home")->name('home');
+Route::get('/help','StaticPagesController@help')->name('help');
+Route::get('/about','StaticPagesController@about')->name('about');
+
+Route::get('signup','UsersController@create')->name('signup');
